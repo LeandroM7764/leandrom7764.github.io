d7f427e5278895eea8ea0fac06f034cf8f079fd6
Leandro <147534836+LeandroM7764@users.noreply.github.com>
2024-02-28 14:48:07 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"; // update the part BETWEEN the "quotes"
-let userName = "YourUsername"; // same here
+let ownerName = "Leandro M"; // update the part BETWEEN the "quotes"
+let userName = "LeandroM7764"; // same here
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
